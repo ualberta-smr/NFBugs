9655b8a7848abf1bbd0ba748d75c4f776bc6ac60
Aida Radu <aradu@cs.ualberta.ca>
2018-05-03 15:33:34 -0600

    repo mining practise that gets commit messages with 'fix'

---
+++ RepoDrillPractise/src/CommitsWithBugVisitor.java
@@ -0,0 +1,31 @@
+// rough copy of mining a sofware repo
+// with credit to Maurício Aniche, https://github.com/mauricioaniche/repodriller 
+import org.repodriller.domain.Commit;
+import org.repodriller.persistence.PersistenceMechanism;
+import org.repodriller.scm.CommitVisitor;
+import org.repodriller.scm.SCMRepository;
+
+public class CommitsWithBugVisitor implements CommitVisitor {
+
+	@Override
+	public void process(SCMRepository repo, Commit commit, PersistenceMechanism writer) {
+		boolean hasFix = commit.getMsg().contains("fix");
+		if (hasFix)
+			{
+			writer.write(
+					commit.getHash(),
+					commit.getMsg()
+
+				);
+			}
+			
+	
+
+	}
+	
+	@Override
+	public String name() {
+		return "commits-with-bugs";
+	}
+}
+
