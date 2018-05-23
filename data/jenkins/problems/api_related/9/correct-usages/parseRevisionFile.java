6201545aaef371f8d98e5b39246449e62f9d23c9
Aida Radu <aradu@cs.ualberta.ca>
2018-05-23 15:34:47 -0600

    update foler content

---
+++ data/jenkins/problems/api_related/9/correct-usages/parseRevisionFile.java
@@ -0,0 +1,101 @@
+package hudson.scm;
+
+import hudson.FilePath;
+import hudson.FilePath.FileCallable;
+import hudson.Launcher;
+import hudson.Util;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
+import hudson.model.Hudson;
+import hudson.model.TaskListener;
+import hudson.remoting.Callable;
+import hudson.remoting.Channel;
+import hudson.remoting.VirtualChannel;
+import hudson.util.FormFieldValidator;
+import hudson.util.IOException2;
+import hudson.util.MultipartFormDataParser;
+import hudson.util.Scrambler;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.io.FileUtils;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Chmod;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.tmatesoft.svn.core.SVNErrorCode;
+import org.tmatesoft.svn.core.SVNErrorMessage;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
+import org.tmatesoft.svn.core.auth.ISVNAuthenticationProvider;
+import org.tmatesoft.svn.core.auth.SVNAuthentication;
+import org.tmatesoft.svn.core.auth.SVNPasswordAuthentication;
+import org.tmatesoft.svn.core.auth.SVNSSHAuthentication;
+import org.tmatesoft.svn.core.auth.SVNSSLAuthentication;
+import org.tmatesoft.svn.core.auth.SVNUserNameAuthentication;
+import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
+import org.tmatesoft.svn.core.internal.io.fs.FSRepositoryFactory;
+import org.tmatesoft.svn.core.internal.io.svn.SVNRepositoryFactoryImpl;
+import org.tmatesoft.svn.core.internal.wc.DefaultSVNAuthenticationManager;
+import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
+import org.tmatesoft.svn.core.wc.SVNClientManager;
+import org.tmatesoft.svn.core.wc.SVNInfo;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.core.wc.SVNUpdateClient;
+import org.tmatesoft.svn.core.wc.SVNWCClient;
+import org.tmatesoft.svn.core.wc.SVNWCUtil;
+
+import javax.servlet.ServletException;
+import javax.xml.transform.stream.StreamResult;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Serializable;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+/*package*/ static Map<String,Long> parseRevisionFile(AbstractBuild build) throws IOException {
+        Map<String,Long> revisions = new HashMap<String,Long>(); // module -> revision
+        {// read the revision file of the last build
+            File file = getRevisionFile(build);
+            if(!file.exists())
+                // nothing to compare against
+                return revisions;
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            try {
+                String line;
+                while((line=br.readLine())!=null) {
+                    int index = line.lastIndexOf('/');
+                    if(index<0) {
+                        continue;   // invalid line?
+                    }
+                    try {
+                        revisions.put(line.substring(0,index), Long.parseLong(line.substring(index+1)));
+                    } catch (NumberFormatException e) {
+                        // perhaps a corrupted line. ignore
+                    }
+                }
+            } finally {
+                br.close();
+            }
+        }
+
+        return revisions;
+    }
+
