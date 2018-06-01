3be617a33e180865fc2b4a75e54cd43cac7d03fd
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-01 11:37:35 -0600

    Create processNextStart().java

---
+++ data/pipes/problems/api-related/1/correct-usages/processNextStart().java
@@ -0,0 +1,28 @@
+package com.tinkerpop.pipes.serial.filter;
+
+import com.tinkerpop.pipes.serial.AbstractPipe;
+import com.tinkerpop.pipes.serial.SingleIterator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+
+public S processNextStart() {
+        while (this.starts.hasNext()) {
+            S s = this.starts.next();
+            boolean and = true;
+            for (FilterPipe<S> pipe : this.pipes) {
+                pipe.setStarts(new SingleIterator<S>(s));
+                if (!pipe.hasNext()) {
+                    and = false;
+                    break;
+                } else {
+                    pipe.clear();
+                }
+            }
+            if (and)
+                return s;
+        }
+        throw new NoSuchElementException();
+    }
