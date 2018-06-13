b28d76e7bd07a3f1c9ec8915f68b95ca7d7c8c89
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-13 13:14:46 -0600

    Create destroy.java

---
+++ java-data/terasology/problems/api_related/5/correct-usages/destroy.java
@@ -0,0 +1,66 @@
+package org.terasology.entitySystem.entity.internal;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.common.collect.MapMaker;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import gnu.trove.iterator.TLongIterator;
+import gnu.trove.iterator.TLongObjectIterator;
+import gnu.trove.list.TLongList;
+import gnu.trove.list.array.TLongArrayList;
+import gnu.trove.set.TLongSet;
+import gnu.trove.set.hash.TLongHashSet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.terasology.entitySystem.Component;
+import org.terasology.entitySystem.entity.EntityBuilder;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.entity.lifecycleEvents.BeforeDeactivateComponent;
+import org.terasology.entitySystem.entity.lifecycleEvents.BeforeEntityCreated;
+import org.terasology.entitySystem.entity.lifecycleEvents.BeforeRemoveComponent;
+import org.terasology.entitySystem.entity.lifecycleEvents.OnActivatedComponent;
+import org.terasology.entitySystem.entity.lifecycleEvents.OnAddedComponent;
+import org.terasology.entitySystem.entity.lifecycleEvents.OnChangedComponent;
+import org.terasology.entitySystem.event.internal.EventSystem;
+import org.terasology.entitySystem.metadata.ComponentLibrary;
+import org.terasology.entitySystem.prefab.Prefab;
+import org.terasology.entitySystem.prefab.PrefabManager;
+import org.terasology.logic.location.LocationComponent;
+import org.terasology.math.geom.Quat4f;
+import org.terasology.math.geom.Vector3f;
+import org.terasology.persistence.typeHandling.TypeSerializationLibrary;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+public class PojoEntityManager implements EngineEntityManager {
+      /**
+     * Destroys this entity, sending event
+     *
+     * @param entityId
+     */
+    @Override
+    public void destroy(long entityId) {
+        // Don't allow the destruction of unloaded entities.
+        if (!loadedIds.contains(entityId)) {
+            return;
+        }
+        EntityRef ref = createEntityRef(entityId);
+        if (eventSystem != null) {
+            eventSystem.send(ref, BeforeDeactivateComponent.newInstance());
+            eventSystem.send(ref, BeforeRemoveComponent.newInstance());
+        }
+        notifyComponentRemovalAndEntityDestruction(entityId, ref);
+        destroy(ref);
+    }
+}
