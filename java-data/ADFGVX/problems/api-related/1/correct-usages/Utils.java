01282e4ad84867bb4e2faf7484f3a2bed621c4ec
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-14 15:16:44 -0600

    Create Utils.java

---
+++ java-data/ADFGVX/problems/api-related/1/correct-usages/Utils.java
@@ -0,0 +1,21 @@
+package ch.hearc.st.app;
+
+import java.util.ArrayList;
+
+public class Utils{
+
+
+  private static String getIndicePermutation(int[] tab) {
+      StringBuilder perm = new StringBuilder();
+      int nbiter = tab.length;
+
+      for (int m = 0; m < nbiter; m++) {
+        perm.append(tab[m]);
+        if (m < nbiter - 1) {
+          perm.append(",");
+        }
+      }
+
+      return perm.toString();
+    }
+}
