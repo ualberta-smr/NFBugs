9d6e7a215f0d1e6830a9dee3c1bd10edc41109e9
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-15 15:48:44 -0600

    Create _create_shingle.py

---
+++ py-data/Text-similarity-class/problems/api-related/2/correct-usages/_create_shingle.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import time
+from sklearn.feature_extraction.text import TfidfVectorizer
+import string
+import warnings
+warnings.filterwarnings('ignore')
+from contextlib import contextmanager
+import mysql.connector
+from sqlalchemy import create_engine
+import pygsheets
+from tqdm import tqdm
+import yaml
+import os
+import binascii
+import random
+from numba import jit
+
+class TextSim(object):
+
+   def _create_shingle(self, doc):
+        shinglesInDoc = set()
+        
+        for index in range(0, len(doc) - (self.shinglesize - 1)):
+            # Construct the shingle text by combining words together, depending on the shingle size passed      
+            shingle = " ".join(doc[index:(index + self.shinglesize)])           
+            # Hash the shingle to a 32-bit integer.
+            shingle = bytes(shingle.strip(), encoding='utf-8')
+            crc = binascii.crc32(shingle) & 0xffffffff
+            # Add the hash value to the set of shingles for the current document. 
+            shinglesInDoc.add(crc)         
+        return shinglesInDoc
