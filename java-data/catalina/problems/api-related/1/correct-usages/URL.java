cdf5dbc201d05534efd1cb4a90b227d9982d2c06
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-15 13:41:21 -0600

    Create URL.java

---
+++ java-data/catalina/problems/api-related/1/correct-usages/URL.java
@@ -0,0 +1,35 @@
+package org.apache.catalina.util;
+
+
+import java.io.Serializable;
+import java.net.MalformedURLException;
+
+public final class URL implements Serializable {
+
+public String toString() {
+
+        StringBuilder sb = new StringBuilder("URL[");
+        sb.append("authority=");
+        sb.append(authority);
+        sb.append(", file=");
+        sb.append(file);
+        sb.append(", host=");
+        sb.append(host);
+        sb.append(", port=");
+        sb.append(port);
+        sb.append(", protocol=");
+        sb.append(protocol);
+        sb.append(", query=");
+        sb.append(query);
+        sb.append(", ref=");
+        sb.append(ref);
+        sb.append(", userInfo=");
+        sb.append(userInfo);
+        sb.append("]");
+        return (sb.toString());
+
+        //        return (toExternalForm());
+
+    }
+
+}
