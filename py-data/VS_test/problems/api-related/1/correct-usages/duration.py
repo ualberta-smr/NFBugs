bf7a4749d4a215c9a05b2908f16ef8feb8b06d98
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-15 15:37:32 -0600

    Create duration.py

---
+++ py-data/VS_test/problems/api-related/1/correct-usages/duration.py
@@ -0,0 +1,10 @@
+def duration(seconds):
+	p, s = divmod(seconds, 60)
+	p, m = divmod(p, 60)
+	p, h = divmod(p, 60)
+	p, d = divmod(p, 24)
+	y = p%365
+	t = ["year", "day", "hour", "minute", "second"]
+	t = [str(j) + " " + t[i] for i,j in enumerate((y,d,h,m,s)) if j != 0]
+	t = [i + "s" for i in t if not i.startswith("1")]
+	return ", ".join(t[:-1]) + " and " + t[-1] if len(t) > 1 else t[0]
