b4a417a2d0522baa57dcba25bf46eb619a50aeb1
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-15 13:25:54 -0600

    Create ConfigUtils.java

---
+++ java-data/dbmaintain/problems/api-related/1/correct-usages/ConfigUtils.java
@@ -0,0 +1,25 @@
+package org.dbmaintain.config;
+
+import org.dbmaintain.util.DbMaintainException;
+import org.dbmaintain.util.ReflectionUtils;
+
+import java.util.Properties;
+
+public class ConfigUtils {
+  
+  private static String getConfiguredClassName(Class<?> type, Properties configuration, String propertyName, String... implementationDiscriminatorValues) {
+        String propKey = type.getName() + "." + propertyName;
+
+        // first try specific instance using the given discriminators
+        if (implementationDiscriminatorValues != null) {
+            StringBuilder implementationSpecificPropKey = new StringBuilder(propKey);
+            for (String implementationDiscriminatorValue : implementationDiscriminatorValues) {
+                implementationSpecificPropKey.append('.').append(implementationDiscriminatorValue);
+            }
+            if (configuration.containsKey(implementationSpecificPropKey.toString())) {
+                return PropertyUtils.getString(implementationSpecificPropKey.toString(), configuration);
+            }
+        }
+    }
+    
+  }
