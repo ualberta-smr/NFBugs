143ccf2928e0ad31f3cba2e0cd40315696e47e5d
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-21 11:35:35 -0600

    minimize usage (close #5) @snadi Thoughts/feedback?

--- java-data/EfficientAdapter/problems/api-related/1/correct-usages/AbsViewHolder.java
+++ java-data/EfficientAdapter/problems/api-related/1/correct-usages/AbsViewHolder.java
@@ -1,4 +1,4 @@
-  package com.skocken.efficientadapter.lib.viewholder;
+package com.skocken.efficientadapter.lib.viewholder;
 
 import android.content.Context;
 import android.content.res.Resources;
@@ -6,24 +6,10 @@ import android.support.v7.widget.RecyclerView;
 import android.util.SparseArray;
 import android.view.View;
 
-
-public void clearViewsCached() {
-        mSparseSparseArrayView.clear();
-    }
-
-    /**
-     * Allow to clear the cache of view retrieved
-     */
-    public void clearViewCached(int viewId) {
-        clearViewCached(0, viewId);
-    }
-
-    /**
-     * Allow to clear the cache of view retrieved
-     */
-    public void clearViewCached(int parentId, int viewId) {
-        SparseArray<View> sparseArrayViewsParent = mSparseSparseArrayView.get(parentId);
-        if (sparseArrayViewsParent != null) {
-            sparseArrayViewsParent.remove(viewId);
-        }
-    }
+public abstract class AbsViewHolder<T> extends RecyclerView.ViewHolder {
+  
+    private void pattern(int parentId, int id, View viewRetrieve) {
+        storeView(parentId, id, viewRetrieve);
+      
+        clearViewCached(parentId, id);
+}
