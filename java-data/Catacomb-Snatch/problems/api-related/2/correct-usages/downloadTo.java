58fb2e9f759cd946b488e06ac4de58499105b2a3
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-21 09:46:41 -0600

    removed some unnecessary code (#9)

--- java-data/Catacomb-Snatch/problems/api-related/2/correct-usages/downloadTo.java
+++ java-data/Catacomb-Snatch/problems/api-related/2/correct-usages/downloadTo.java
@@ -16,36 +16,12 @@ public class ChannelDownloader implements IDownloader {
 
 	@Override
 	public void downloadTo(String turl, String dest) throws IOException {
-		URL url = new URL(turl);
-
-		URLConnection urlC = url.openConnection();
-
-		// allow both GZip and Deflate (ZLib) encodings
-
-		urlC.setRequestProperty("Accept-Encoding", "gzip, deflate");
-
-		// set the user agent to pass Cloud-Flare
-
-		urlC.setRequestProperty("User-agent", Downloader.getUserAgent());
-
-		
-		String encoding = urlC.getContentEncoding();
-
-		InputStream is = null;
-
-		if (encoding != null && encoding.equalsIgnoreCase("gzip")) {
-			is = new GZIPInputStream(urlC.getInputStream());
-		} else if (encoding != null && encoding.equalsIgnoreCase("deflate")) {
-			is = new InflaterInputStream(urlC.getInputStream(), new Inflater(true));
-		} else {
-			is = urlC.getInputStream();
-		}
 
 		ReadableByteChannel rbc = Channels.newChannel(is);
 		
 		FileOutputStream fos = new FileOutputStream(dest);
 		fos.getChannel().transferFrom(rbc, 0, 1 << 24);
-		// DownloadScreen.drawGraph(fileSize/2, fileSize);
+
 		fos.close();
 		return;
 	}
