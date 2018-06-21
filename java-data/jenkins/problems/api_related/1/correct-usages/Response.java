09963e0e940467618258743fe77067ac5d28f0bc
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-21 14:31:43 -0600

    added class name

---
+++ java-data/jenkins/problems/api_related/1/correct-usages/Response.java
@@ -0,0 +1,12 @@
+package hudson.remoting;
+
+final class Response<RSP,EXC extends Throwable> extends Command {
+  @Override
+     protected void execute(Channel channel) {
+         Request req = channel.pendingCalls.get(id);
+         if(req==null)
+             return; // maybe aborted
+         req.onCompleted(this);
+         channel.pendingCalls.remove(id);
+     }
+}
