cbc855c27119959b24ef655df5cebe50ba7f2060
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-21 16:10:25 -0600

    minimized (#9)

--- java-data/jenkins/problems/api_related/13/correct-usages/PluginWrapper.java
+++ java-data/jenkins/problems/api_related/13/correct-usages/PluginWrapper.java
@@ -27,99 +27,20 @@ import java.util.List;
 import java.util.jar.Manifest;
 import java.util.logging.Logger;
 
-public PluginWrapper(PluginManager owner, File archive) throws IOException {
-        LOGGER.info("Loading plugin: "+archive);
-        this.archive = archive;
+public final class PluginWrapper {
+        private static final class Dependency {
+                public PluginWrapper(PluginManager owner, File archive) throws IOException {
 
-        boolean isLinked = archive.getName().endsWith(".hpl");
+                        boolean isLinked = archive.getName().endsWith(".hpl");
 
-        File expandDir = null;  // if .hpi, this is the directory where war is expanded
-
-        if(isLinked) {
-            // resolve the .hpl file to the location of the manifest file
-            BufferedReader archiveReader = new BufferedReader(new FileReader(archive));
-            try {
-                String firstLine = archiveReader.readLine();
-                if(firstLine.startsWith("Manifest-Version:")) {
-                    // this is the manifest already
-                } else {
-                    // indirection
-                    archive = resolve(archive, firstLine);
-                }
-            } finally {
-                archiveReader.close();
-            }
-            // then parse manifest
-            FileInputStream in = new FileInputStream(archive);
-            try {
-                manifest = new Manifest(in);
-            } catch(IOException e) {
-                throw new IOException2("Failed to load "+archive,e);
-            } finally {
-                in.close();
-            }
-        } else {
-            expandDir = new File(archive.getParentFile(), getBaseName(archive));
-            explode(archive,expandDir);
-
-            File manifestFile = new File(expandDir,"META-INF/MANIFEST.MF");
-            if(!manifestFile.exists()) {
-                throw new IOException("Plugin installation failed. No manifest at "+manifestFile);
-            }
-            FileInputStream fin = new FileInputStream(manifestFile);
-            try {
-                manifest = new Manifest(fin);
-            } finally {
-                fin.close();
-            }
+                        if(isLinked) {
+                            BufferedReader archiveReader = new BufferedReader(new FileReader(archive));
+                            try {
+                                // ...
+                            } finally {
+                                archiveReader.close();
+                            }
+                        }
+                    }
         }
-
-        this.shortName = computeShortName(manifest,archive);
-
-        // TODO: define a mechanism to hide classes
-        // String export = manifest.getMainAttributes().getValue("Export");
-
-        List<URL> paths = new ArrayList<URL>();
-        if(isLinked) {
-            parseClassPath(archive, paths, "Libraries", ",");
-            parseClassPath(archive, paths, "Class-Path", " +"); // backward compatibility
-
-            this.baseResourceURL = resolve(archive,
-                manifest.getMainAttributes().getValue("Resource-Path")).toURL();
-        } else {
-            File classes = new File(expandDir,"WEB-INF/classes");
-            if(classes.exists())
-                paths.add(classes.toURL());
-            File lib = new File(expandDir,"WEB-INF/lib");
-            File[] libs = lib.listFiles(JAR_FILTER);
-            if(libs!=null) {
-                for (File jar : libs)
-                    paths.add(jar.toURL());
-            }
-
-            this.baseResourceURL = expandDir.toURL();
-        }
-        ClassLoader dependencyLoader = new DependencyClassLoader(getClass().getClassLoader(),owner);
-        this.classLoader = new URLClassLoader(paths.toArray(new URL[0]), dependencyLoader);
-
-        disableFile = new File(archive.getPath()+".disabled");
-        if(disableFile.exists()) {
-            LOGGER.info("Plugin is disabled");
-            this.active = false;
-        } else {
-            this.active = true;
-        }
-
-        // compute dependencies
-        String v = manifest.getMainAttributes().getValue("Plugin-Dependencies");
-        if(v!=null) {
-            for(String s : v.split(",")) {
-                Dependency d = new Dependency(s);
-                if (d.optional) {
-                    optionalDependencies.add(d);
-                } else {
-                    dependencies.add(d);
-                }
-            }
-        }
-    }
+}
