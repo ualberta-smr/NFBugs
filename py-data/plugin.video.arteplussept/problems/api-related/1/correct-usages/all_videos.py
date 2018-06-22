04a1cbdadcd97ce4c559952c3d537686fb7e0cdc
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-22 15:50:41 -0600

    Update all_videos.py

--- py-data/plugin.video.arteplussept/problems/api-related/1/correct-usages/all_videos.py
+++ py-data/plugin.video.arteplussept/problems/api-related/1/correct-usages/all_videos.py
@@ -9,10 +9,5 @@ import datetime
 @plugin.route('/all', name='all')
 def all_videos():
     plugin.set_content('tvshows')
-
-    # create list item & filter programs without 'video' (live streams ?)
     items = [create_item(program) for program in get_last7days() if program.get('video') is not None]
-    # sort by airdate desc
-    items.sort(key=lambda item: item['info']['aired'], reverse=True)
-
     return plugin.finish(items)
