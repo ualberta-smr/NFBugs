627d17c9fd3b95bdde284bfb4efbd79a6076315c
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-22 15:28:23 -0600

    minimize (#9)

--- py-data/backend-interpreter/problems/api-related/1/correct-usages/RethinkInterface.py
+++ py-data/backend-interpreter/problems/api-related/1/correct-usages/RethinkInterface.py
@@ -8,37 +8,11 @@ import rethinkdb
 
 class RethinkInterface:
 
-  def _send_output(self, output_data):
+  def pattern(self, output_data):
         
-        try:
-            output_job = rethinkdb.db("Brain").table("Jobs").get(
-                output_data[0]
-            ).run(self.rethink_connection)
-        except rethinkdb.ReqlDriverError as ex:
-            self.logger.send(["dbprocess",
-                "".join(("Could not access Jobs Table: ", str(ex))),
-                30,
-                time()
-            ])
-        #if the job has an entry add the output to the output table
-        if output_job != None:
-            output_entry = {
-                "OutputJob": output_job,
-                "Content": output_data[1]
-            }
-            try:
-                rethinkdb.db("Brain").table("Outputs").insert(
-                    output_entry,
-                    conflict="replace"
-                ).run(self.rethink_connection)
-            except rethinkdb.ReqlDriverError as ex:
-                self.logger.send(["dbprocess",
-                    "".join(("Could not write output to database", str(ex))),
-                    30,
-                    time()
-                ])
-        else:
-            self.logger.send(["dbprocess",
+        # ...
+        
+        self.logger.send(["dbprocess",
                 "".join(("There is no job with an id of ", output_data[0])),
                 30,
                 time()
