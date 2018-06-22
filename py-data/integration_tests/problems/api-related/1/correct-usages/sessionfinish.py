6343939b03471c909a277f03683d9d6966ea5e26
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-22 15:33:27 -0600

    minimize (#9)

--- py-data/integration_tests/problems/api-related/1/correct-usages/sessionfinish.py
+++ py-data/integration_tests/problems/api-related/1/correct-usages/sessionfinish.py
@@ -7,12 +7,5 @@ from cfme.utils.appliance import get_or_create_current_appliance
 
 def pytest_sessionfinish(session, exitstatus):
     c = collections.Counter()
-    for test in test_tracking:
-        c[_test_status(test)] += 1
-    # Prepend a total to the summary list
     results = ['total: {}'.format(sum(c.values()))] + [
         '{}: {}'.format(k, v) for k, v in c.items()]
-    # Then join it with commas
-    summary = ', '.join(results)
-    logger().info(log.format_marker('Finished test run', mark='='))
-    logger().info(log.format_marker(str(summary), mark='='))
