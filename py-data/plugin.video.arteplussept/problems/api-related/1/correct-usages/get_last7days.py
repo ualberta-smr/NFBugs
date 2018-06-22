2aa4100ce852879b9482d112ffe65a5b7cc17828
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-22 15:52:20 -0600

    flatten wasn't part of this specific change
    -- removed it

--- py-data/plugin.video.arteplussept/problems/api-related/1/correct-usages/get_last7days.py
+++ py-data/plugin.video.arteplussept/problems/api-related/1/correct-usages/get_last7days.py
@@ -5,9 +5,6 @@ import os
 import urllib2
 import time
 import datetime
-
-def flatten(l):
-  return [item for sublist in l for item in sublist]
   
 def get_last7days():
     return flatten([get_day(date) for (date, _) in get_dates()])
