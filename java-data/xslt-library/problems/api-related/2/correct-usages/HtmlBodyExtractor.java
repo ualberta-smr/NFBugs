73f279b7805fafc5f230371be67dd4a59197df2f
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-22 13:18:50 -0600

    clean up a little (#9)

--- java-data/xslt-library/problems/api-related/2/correct-usages/HtmlBodyExtractor.java
+++ java-data/xslt-library/problems/api-related/2/correct-usages/HtmlBodyExtractor.java
@@ -12,25 +12,18 @@ import javax.annotation.Nonnull;
 
 public class HtmlBodyExtractor {
   
-  protected void extractElements(
-        @Nonnull StringBuilder result, @Nonnull String input,
-        @Nonnull List<Range> ranges,
-        @Nonnull String start, @CheckForNull String endOfStartOrNull, @Nonnull String end
-    ) {
+  protected void extractElements(@Nonnull StringBuilder result, @Nonnull String input, @Nonnull List<Range> ranges,
+        @Nonnull String start, @CheckForNull String endOfStartOrNull, @Nonnull String end) {
+        
         int startIdx = -1;
         val ourResult = new StringBuilder();
+    
         while ((startIdx = input.indexOf(start, startIdx+1)) >= 0) {
-            int idxOfEndTag = input.indexOf(end, startIdx);
-            Range range = endOfStartOrNull == null 
+            Range range = (endOfStartOrNull == null)
                 ? new Range(startIdx, idxOfEndTag + end.length())
                 : new Range(input.indexOf(endOfStartOrNull, startIdx) + endOfStartOrNull.length(), idxOfEndTag);
-            boolean rangeOverlaps = false;
-            for (Range r : ranges) if (r.overlaps(range)) rangeOverlaps = true;
-            if (rangeOverlaps) continue;
             ourResult.append(ignoreScripts(input.substring(range.startIncl, range.endExcl)));
-            ranges.add(range);
         }
         ourResult.append("\n");
-        result.insert(0, ourResult);
     }
   }
