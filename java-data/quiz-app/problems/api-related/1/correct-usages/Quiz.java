5fc352bd982c680ed959c01cebcd8c2764bcc247
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-06-25 13:22:11 -0600

    Create Quiz.java

---
+++ java-data/quiz-app/problems/api-related/1/correct-usages/Quiz.java
@@ -0,0 +1,19 @@
+package com.developervishalsehgal.udacityscholarsapp.data.models;
+
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+@IgnoreExtraProperties
+public class Quiz {
+  
+  private double mRating;
+  
+  public double getRating() {
+        return mRating;
+    }
+
+}
