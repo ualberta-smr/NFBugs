6b38b4c8ed9a0ee50c3a2bea2409502fce11219c
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-07-10 14:58:17 -0600

    made a better test file and made it prettier

--- GumCode/src/main/java/getActions.java
+++ GumCode/src/main/java/getActions.java
@@ -5,20 +5,56 @@ import com.github.gumtreediff.actions.*;
 import com.github.gumtreediff.matchers.*;
 import com.github.gumtreediff.actions.model.*;
 import com.github.gumtreediff.tree.*;
+import com.github.gumtreediff.tree.ITree;
+import com.github.gumtreediff.tree.hash.HashUtils.*;
 import java.util.List;
 import java.io.IOException;
 
-public class TreeAction{
+import javassist.compiler.ast.ASTree;
+import org.eclipse.jdt.core.dom.*;
+import com.github.gumtreediff.gen.jdt.*;
+import java.io.PrintWriter;
+
+public class TreeActions{
     public static void main (String[] args) throws IOException{
         Run.initGenerators();
-        String file1 = "/Users/radu/Documents/HelloWorld.java";
-        String file2 = "/Users/radu/Documents/Hello.java";
+        String file1 = "/Users/radu/Documents/Test2.java";
+        String file2 = "/Users/radu/Documents/TestEDITED2.java";
         ITree src = Generators.getInstance().getTree(file1).getRoot();
         ITree dst = Generators.getInstance().getTree(file2).getRoot();
-        Matcher m = Matchers.getInstance().getMatcher(src, dst); // retrieve the default matcher
-        m.match();
-        ActionGenerator g = new ActionGenerator(src, dst, m.getMappings());
-        g.generate();
-        List<Action> actions = g.getActions(); // return the actions
+        PrintWriter writer = new PrintWriter("GumOut.txt","UTF-8");
+        try {
+
+            writer.println("------------------------ TREE of ORIGINAL ---------------------------");
+            writer.println(src.toTreeString());
+
+
+
+
+            writer.println("------------------------ NEW TREE ---------------------------");
+            writer.println(dst.toTreeString());
+
+            Matcher m = Matchers.getInstance().getMatcher(src, dst); // retrieve the default matcher
+            m.match();
+            ActionGenerator g = new ActionGenerator(src, dst, m.getMappings());
+            g.generate();
+            List<Action> actions = g.getActions(); // return the actions
+
+
+
+            writer.println("------------------- ACTIONS BETWEEN TREES --------------------------------------\n");
+            for (int i = 0; i < actions.size(); i++) {
+                writer.println(actions.get(i).toString());
+            }
+
+
+            writer.println("----------------------------------------------------------------");
+            writer.println("We want the FULLY QUALIFIED version of:");
+            writer.println(dst.getDescendants().get(10).getLabel());
+        } finally {
+            writer.close();
+
+        }
+
     }
 }
