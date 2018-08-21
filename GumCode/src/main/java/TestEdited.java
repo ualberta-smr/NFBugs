f7d09a1c56315ef96a07133b8ab2a9c864ecab6e
aradu12 <35245025+aradu12@users.noreply.github.com>
2018-08-21 11:49:14 -0600

    Create TestEdited.java

---
+++ GumCode/src/main/java/TestEdited.java
@@ -0,0 +1,8 @@
+class TestEdited{
+	public static void main(String[] args){
+		StringBuilder myString = new StringBuilder();
+		myString.append("Hello");
+		myString.append(" World!");
+   		System.out.println(myString);
+   }
+}
